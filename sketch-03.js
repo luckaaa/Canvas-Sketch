3fef59b8671886d74e99ee30dbbd721f0949e1bf
luka <98187912+luckaaa@users.noreply.github.com>
2022-05-27 13:57:45 +0200

    Update sketch-03.js

--- sketch-03.js
+++ sketch-03.js
@@ -1,101 +1,103 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
-const math = require('canvas-sketch-util/math');
 
 const settings = {
-	dimensions: [ 1080, 1080 ],
-	animate: true
+  dimensions: [ 2048, 2048 ],
+  animate: true
 };
 
-const animate = () => {
-	requestAnimationFrame(animate);
-};
-// animate();
+const colors = ["#30cfd0", "#330867", "#a8edea", "#fed6e3", "#5ee7df", "#b490ca"]
 
 const sketch = ({ context, width, height }) => {
-	const agents = [];
 
-	for (let i = 0; i < 40; i++) {
-		const x = random.range(0, width);
-		const y = random.range(0, height);
+  const agents = [];
 
-		agents.push(new Agent(x, y));
-	}
+  for (let i = 0; i < 80; i++){
+    const x = random.range(0, width);
+    const y = random.range(0, height);
 
-	return ({ context, width, height }) => {
-		context.fillStyle = 'white';
-		context.fillRect(0, 0, width, height);
+    agents.push(new Agent(x,y))
+  }
 
-		for (let i = 0; i < agents.length; i++) {
-			const agent = agents[i];
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
 
-			for (let j = i + 1; j < agents.length; j++) {
-				const other = agents[j];
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
 
-				const dist = agent.pos.getDistance(other.pos);
+      for (let j = 0 + i; j < agents.length; j++) {
+        const other = agents[j];
 
-				if (dist > 200) continue;
+        const dist = agent.pos.getDistance(other.pos);
 
-				context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+        if (dist > 200) continue;
 
-				context.beginPath();
-				context.moveTo(agent.pos.x, agent.pos.y);
-				context.lineTo(other.pos.x, other.pos.y);
-				context.stroke();
-			}
-		}
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke()
+      }
+    }
 
-		agents.forEach(agent => {
-			agent.update();
-			agent.draw(context);
-			agent.bounce(width, height);
-		});
-	};
+    agents.forEach(agent => {
+      agent.update()
+      agent.draw(context);
+      agent.wrap(width, height);
+    });
+  }
 };
 
 canvasSketch(sketch, settings);
 
 class Vector {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
+  constructor(x, y, radius){
+    this.x = x;
+    this.y = y;
+  }
 
-	getDistance(v) {
-		const dx = this.x - v.x;
-		const dy = this.y - v.y;
-		return Math.sqrt(dx * dx + dy * dy);
-	}
+  getDistance(v){
+    const dx = this.x - v.x
+    const dy = this.y - v.y
+    return Math.sqrt(dx * dx + dy * dy)
+  }
 }
 
 class Agent {
-	constructor(x, y) {
-		this.pos = new Vector(x, y);
-		this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
-		this.radius = random.range(4, 12);
-	}
+  constructor(x, y){
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4,12);
+  }
 
-	bounce(width, height) {
-		if (this.pos.x <= 0 || this.pos.x >= width)  this.vel.x *= -1;
-		if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
-	}
+  bounce(width, height){
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1.2;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1.2;
+  }
+     
+  wrap(width, height){
+    if (this.pos.x > width) this.pos.x = 0;
+  }
 
-	update() {
-		this.pos.x += this.vel.x;
-		this.pos.y += this.vel.y;
-	}
+  update(){
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
 
-	draw(context) {
-		context.save();
-		context.translate(this.pos.x, this.pos.y);
+  draw(context){
+    //var color = colors[Math.floor(Math.random()*5)]
 
-		context.lineWidth = 4;
+    context.save()
+    context.translate(this.pos.x, this.pos.y)
 
-		context.beginPath();
-		context.arc(0, 0, this.radius, 0, Math.PI * 2);
-		context.fill();
-		context.stroke();
+    context.lineWidth = 4
 
-		context.restore();
-	}
+    context.beginPath();
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.fillStyle = "black"
+    context.fill();
+
+    context.restore();
+  }
 }
+
